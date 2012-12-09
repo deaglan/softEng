819947cf537a4da64c6f22f35e46a8f9018cafbd
declanmcdonnell <declanmcdonnell@outlook.com>
2012-12-09 15:46:24 +0000

    "text wrapping on Answer panel on essay & math tab"

--- src/ex1/TabbedPane.java
+++ src/ex1/TabbedPane.java
@@ -1,5 +1,21 @@
 package ex1;
 
+/**
+ * @author Deaglan
+ * This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
 import java.awt.BorderLayout;
 import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
@@ -227,8 +243,8 @@ public class TabbedPane extends JPanel {
 		jp.add(jtQuestion, "wrap");
 
 		JLabel jlCorrectAnswer = new JLabel("Correct Answer");
-		final JTextArea jtAns = new JTextArea("", 0,10);
-
+		final JTextArea jtAns = new JTextArea("", 10,10);
+		jtAns.setLineWrap(true);
 		jp.add(jlCorrectAnswer);
 		jp.add(jtAns, "grow,wrap");
 
